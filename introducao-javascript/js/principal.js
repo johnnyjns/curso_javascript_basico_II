54d86d31712da8b8a8e35c8cd9f3240a5ec50d74
Johnny Souto <johnnyjns@gmail.com>
2017-03-14 18:16:40 -0300

    fim do captiulo-02

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -7,14 +7,32 @@ var tdAltura = paciente.querySelector(".info-altura");
 var tdImc = paciente.querySelector(".info-imc");
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-var imc = peso / (altura * altura);
 
-tdImc.textContent = imc;
+if (peso <= 0 || peso >= 1000) {
+  pesoEhValido = false;
+  console.log("Peso inválido!");
+  tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+  alturaEhValida = false;
+  console.log("Altura inválida!");
+  tdImc.textContent = "Altura inválida";
+}
+
+if (pesoEhValido && alturaEhValida) {
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+  console.log(imc);
+} else {
+  console.log("Não foi calculado o imc!");
+}
 
 console.log(paciente);
 console.log(tdPeso);
 console.log(tdImc);
 console.log(peso);
 console.log(altura);
-console.log(imc);
