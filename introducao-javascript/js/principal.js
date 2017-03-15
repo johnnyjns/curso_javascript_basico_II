894215d3a2f9a9df80f2975dd6c020a2debd5c25
Johnny Souto <johnnyjns@gmail.com>
2017-03-14 21:54:05 -0300

    fim do capitulo-3

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,38 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
-var pesoEhValido = true;
-var alturaEhValida = true;
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+  console.log(pacientes[i]);
+  var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var tdAltura = paciente.querySelector(".info-altura");
+  var tdImc = paciente.querySelector(".info-imc");
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
+  var pesoEhValido = true;
+  var alturaEhValida = true;
 
 
-if (peso <= 0 || peso >= 1000) {
-  pesoEhValido = false;
-  console.log("Peso inválido!");
-  tdImc.textContent = "Peso inválido";
+  if (peso <= 0 || peso >= 1000) {
+    pesoEhValido = false;
+    console.log("Peso inválido!");
+    tdImc.textContent = "Peso inválido";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (altura <= 0 || altura >= 3.00) {
+    alturaEhValida = false;
+    console.log("Altura inválida!");
+    tdImc.textContent = "Altura inválida";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+    console.log(imc);
+  } else {
+    console.log("Não foi calculado o imc!");
+  }
 }
-
-if (altura <= 0 || altura >= 3.00) {
-  alturaEhValida = false;
-  console.log("Altura inválida!");
-  tdImc.textContent = "Altura inválida";
-}
-
-if (pesoEhValido && alturaEhValida) {
-  var imc = peso / (altura * altura);
-  tdImc.textContent = imc;
-  console.log(imc);
-} else {
-  console.log("Não foi calculado o imc!");
-}
-
-console.log(paciente);
-console.log(tdPeso);
-console.log(tdImc);
-console.log(peso);
-console.log(altura);
